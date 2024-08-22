36c4801a7b10e82c22280c00a86a7b5836335e1a
almktr <mktr.acc@gmail.com>
2024-08-22 23:40:14 +0400

    ⚙️  feat(core): add the main configuration file for the API

---
+++ backend/api/core/config.py
@@ -0,0 +1,32 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_core import MultiHostUrl
+from pydantic import computed_field, PostgresDsn
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file="../.env", env_ignore_empty=True, extra="ignore"
+    )
+
+    DB_USER: str
+    DB_PASS: str
+    DB_HOST: str
+    DB_PORT: int
+    DB_NAME: str
+    ALGORITHM: str
+    SECRET_KEY: str
+
+    @computed_field
+    @property
+    def DB_URI(self) -> PostgresDsn:
+        return MultiHostUrl.build(
+            scheme="postgresql+psycopg",
+            username=self.DB_USER,
+            password=self.DB_PASS,
+            host=self.DB_HOST,
+            port=self.DB_PORT,
+            path=self.DB_NAME,
+        )
+
+
+settings = Settings()
