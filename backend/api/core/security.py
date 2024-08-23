e54739d4c25da1390322adce6ece1245df946778
almktr <mktr.acc@gmail.com>
2024-08-23 17:02:45 +0400

    🔒 feat(core): add method to create jwt access token

--- backend/api/core/security.py
+++ backend/api/core/security.py
@@ -1,6 +1,20 @@
+from datetime import datetime, timedelta, timezone
+
+import jwt
 from passlib.context import CryptContext
 
+from .config import settings
+
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def create_access_token(
+    subject: str,
+    expires_delta: timedelta = timedelta(minutes=settings.ACCESS_TOKEN_EXPI_MIN),
+):
+    expire = datetime.now(timezone.utc) + expires_delta
+    payload = {"sub": subject, "exp": expire}
+    return jwt.encode(payload, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
 
 
 def get_password_hash(password: str):
