e215080f124be16ed39761abedc9f0f92fed8f7d
almktr <mktr.acc@gmail.com>
2024-08-23 18:19:41 +0400

    👮‍♂️ feat(core): add methods for authenticating users and getting user roles

--- backend/api/core/security.py
+++ backend/api/core/security.py
@@ -1,11 +1,23 @@
 from datetime import datetime, timedelta, timezone
+from typing import Annotated
 
 import jwt
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jwt.exceptions import InvalidTokenError
 from passlib.context import CryptContext
+from sqlalchemy.orm import Session
+
+from api.user.models import Roles, User
 
 from .config import settings
+from .database import db_dependency
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl=f"{settings.API_VER_STR}/auth/access-token/"
+)
+oauth2_dependency = Annotated[str, Depends(oauth2_scheme)]
 
 
 def create_access_token(
@@ -23,3 +35,46 @@ def get_password_hash(password: str):
 
 def verify_password(plain_password: str, hashed_password: str):
     return pwd_context.verify(plain_password, hashed_password)
+
+
+def authenticate_user(db: Session, username: str, password: str):
+    db_user = db.query(User).filter(User.username == username).first()
+
+    if not db_user:
+        raise ValueError("User not found")
+
+    if not verify_password(password, db_user.password):
+        raise ValueError("Incorrect username or password")
+
+    if not db_user.is_active:
+        raise ValueError("User is not active")
+
+    return db_user
+
+
+def current_user(db: db_dependency, token: oauth2_dependency):
+    try:
+        payload = jwt.decode(
+            token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
+        )
+        token_data = payload.get("sub")
+    except InvalidTokenError:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Could not validate credentials",
+        )
+    user = db.get(User, int(token_data))
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    if not user.is_active:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return user
+
+
+CurrentUser = Annotated[User, Depends(current_user)]
+
+
+def admin_user(current_user: CurrentUser):
+    if not current_user.role == Roles.ADMIN:
+        raise HTTPException(status_code=403, detail="Not enough permissions")
+    return current_user
