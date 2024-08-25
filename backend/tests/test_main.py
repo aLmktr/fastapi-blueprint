f43c2965e505080640dee332c59b9f946f23d51c
almktr <mktr.acc@gmail.com>
2024-08-25 07:34:22 +0400

    ✂️ chore: remove unnecessary  root route test

--- backend/tests/test_main.py
+++
@@ -1,8 +0,0 @@
-def test_read_root(client):
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {
-        "status": "ok",
-        "version": "0.0.1",
-        "documentation": "/docs",
-    }
