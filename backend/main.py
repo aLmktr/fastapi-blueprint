6e79575a92d9b9357c7e4bd83d221d0bdcbaa51a
almktr <mktr.acc@gmail.com>
2024-08-27 22:13:55 +0400

    ✂️ refactor(main): remove unused root endpoint

--- backend/main.py
+++ backend/main.py
@@ -5,14 +5,5 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {
-        "status": "ok",
-        "version": "0.0.1",
-        "documentation": "/docs",
-    }
-
-
 app.include_router(auth_routes.router)
 app.include_router(user_routes.router)
